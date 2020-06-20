62e87ef05b5c51f10ba1ddd7caae851ee0de48e4
gustavo-shigueo <gustavo.gsmn@gmail.com>
2020-06-20 17:01:33 -0300

    Fix attempt #26

--- puck.js
+++ puck.js
@@ -16,25 +16,27 @@ function Puck() {
         this.yspeed = this.velocity * Math.sin(this.angle);
 
         this.l = false;
+        futureEval = false;
         if (random(1) > 0.5) {
+            futureEval = true;
             this.xspeed *= -1;
             this.l = true;
         }
-
+        
     }
-
+    
     this.update = function () {
         this.x += this.xspeed;
         this.y += this.yspeed;
         this.ytop = (this.y - this.r) + this.dificulty * this.yspeed;
         this.ybottom = (this.y + this.r) + this.dificulty * this.yspeed;
     }
-
+    
     this.show = function () {
         fill(255);
         circle(this.x, this.y, this.r);
     }
-
+    
     this.edges = function () {
         if ((this.y <= this.r && this.yspeed < 0) || (this.y >= height - this.r && this.yspeed > 0)) {
             this.yspeed *= -1;
@@ -50,7 +52,7 @@ function Puck() {
             this.reset();
         }
     }
-
+    
     this.checkPaddle = function (p, isLeft) {
         this.testytop = this.y - this.r < p.ybottom;
         this.testybottom = this.y + this.r > p.ytop;
@@ -60,24 +62,26 @@ function Puck() {
         this.hitAngle = map(this.diff, 0, p.h, -this.rad, this.rad);
         if (isLeft && this.l) {
             this.testx = this.x - this.r <= p.xhit;
-
+            
             if (this.testx && this.testybottom && this.testytop) {
                 this.l = false;
+                futureEval = false;
                 this.xspeed = this.velocity * cos(this.hitAngle);
                 this.yspeed = this.velocity * sin(this.hitAngle);
             }
         }
-
+        
         else if (!isLeft && !this.l) {
             this.testx = this.x + this.r >= p.xShow;
-
+            
             if (this.testx && this.testybottom && this.testytop) {
+                futureEval = true;
                 this.l = true;
                 this.xspeed = -this.velocity * cos(this.hitAngle);
                 this.yspeed = this.velocity * sin(this.hitAngle);
             }
         }
-
+        
     }
-
+    
 }
